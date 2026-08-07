1553b4cff41600d89f427af5cb2b8a0cd34be815
indrafetch09 <harindrawahyu131@gmail.com>
2026-08-07 08:15:09 +0700

    fixed: fix dbname

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'sisrw21',
+        'dbname' => 'rw021',
         'charset' => 'utf8mb4'
     ],
 
