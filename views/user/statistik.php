7ca07f9a2e9ddd4a38f22c96d1c052ca6446f32f
indrafetch09 <harindrawahyu131@gmail.com>
2026-08-15 15:03:08 +0700

    refactor: separate head partials, and add logo rw into favicon

--- views/user/statistik.php
+++ views/user/statistik.php
@@ -2,14 +2,9 @@
 <html lang="id">
 
 <head>
-    <<<<<<< HEAD
-        <title>Statistik Demografi Warga - Ruang Warga 021</title>
-        <?php require base_path('views/partials/head.php'); ?>
-        =======
-        <?php $title = "Statistik Demografi Warga - Ruang Warga 021";
-        require base_path('views/partials/head.php'); ?>
-        >>>>>>> main
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <?php $title = "Statistik Demografi Warga - Ruang Warga 021";
+    require base_path('views/partials/head.php'); ?>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body class="bg-gray-50 flex flex-col min-h-screen">
